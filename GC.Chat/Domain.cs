dd2d1bc32e2e94aabbd2a96e9502d6d927a0bd01
Mukund Raghav Sharma (Moko) <mukund.sharma92@gmail.com>
2023-05-08 23:24:55 -0700

    Started real chat bot

---
+++ GC.Chat/Domain.cs
@@ -0,0 +1,25 @@
+﻿namespace GC.Chat
+{
+    /*
+     * 
+      {"files_mentioned":[],"input_prompt":"What is pinning?","performance_issue_type":"","prompt_types":["question"],"response_by_model":" Pinning is a process that indicates to the GC that an object cannot be moved. It is used to prevent fra{files_mentioned:[],input_prompt:What is pinning?,performance_issue_type:,prompt_types:[question],response_by_model: Pinning is a
+process that indicates to the GC that an object cannot be moved. It is used to prevent fragmentation in the heap and to help the GC
+manage memory more efficiently.}
+     */
+    public sealed class SourceResponse 
+    {
+        public string question { get; set; }
+        public string sources { get; set; }
+        public string answer { get; set; }
+    }
+
+    public sealed class ModelResponse
+    {
+        public List<string> files_mentioned { get; set; }
+        public string input_prompt { get; set; }
+        public string response_by_model { get; set; }
+        public string performance_issue_type { get; set; }
+        public List<string> prompt_types { get; set; }
+        public string process_name { get; set; }
+    }
+}
--- GC.Chat/Program.cs
+++ GC.Chat/Program.cs
@@ -4,8 +4,9 @@ using Spectre.Console;
 using System.Text;
 using Microsoft.Diagnostics.Tracing.Analysis;
 using Microsoft.Diagnostics.Tracing;
+using GC.Chat;
 
-static async Task<string> SendRequest(string query)
+static async Task<string> SendMemDocQuestionRequest(string query)
 {
     Dictionary<string, string> input = new();
     input["Query"] = query;
@@ -13,7 +14,25 @@ static async Task<string> SendRequest(string query)
     var json = JsonConvert.SerializeObject(input);
     var data = new StringContent(json, Encoding.UTF8, "application/json");
 
-    var url = "http://mrsharm.pythonanywhere.com/query";
+    var url = "http://mrsharm.pythonanywhere.com/querywithsource";
+    using var client = new HttpClient();
+
+    var response = await client.PostAsync(url, data);
+
+    var result = await response.Content.ReadAsStringAsync();
+    SourceResponse re = JsonConvert.DeserializeObject<SourceResponse>(result);
+    return $"{re.answer}. Sources: [{re.sources}]";
+}
+
+static async Task<string> SendModelRequest(string query)
+{
+    Dictionary<string, string> input = new();
+    input["Query"] = query;
+
+    var json = JsonConvert.SerializeObject(input);
+    var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+    var url = "http://mrsharm.pythonanywhere.com/gc_modelquery";
     using var client = new HttpClient();
 
     var response = await client.PostAsync(url, data);
@@ -38,6 +57,12 @@ void HandleAnalyze(string input)
         return;
     }
 
+    AnalyzeTrace(tracePath);
+}
+
+static void AnalyzeTrace(string tracePath)
+{
+    tracePath = tracePath.Replace("\"", "");
     string extension = Path.GetExtension(tracePath);
 
     if (extension != ".etl" && extension != ".etlx" && !tracePath.EndsWith(".etl.zip"))
@@ -86,6 +111,73 @@ void HandleAnalyze(string input)
     }
 
     AnsiConsole.Write(table);
+    AnsiConsole.WriteLine();
+}
+
+static void AnalyzeProcess(string tracePath, string processName)
+{
+    tracePath = tracePath.Replace("\"", "");
+    processName = processName.Replace("\"", "");
+
+    if (Path.GetExtension(tracePath).Contains(".zip"))
+    {
+        ZippedETLReader reader = new(tracePath);
+        reader.UnpackArchive();
+        tracePath = reader.EtlFileName;
+    }
+
+    Etlx.TraceLog traceLog = Etlx.TraceLog.OpenOrConvert(tracePath);
+    var eventSource = traceLog.Events.GetSource();
+    eventSource.NeedLoadedDotNetRuntimes();
+    eventSource.Process();
+
+    TraceProcess? processToFind = null;
+
+    foreach (var p in eventSource.Processes())
+    {
+        var managedProcess = p.LoadedDotNetRuntime();
+        if (managedProcess == null || managedProcess.GC == null || managedProcess.GC.GCs == null || managedProcess.GC.GCs.Count <= 0)
+        {
+            continue;
+        }
+
+        if (p.Name.ToLower() == processName.ToLower())
+        {
+            processToFind = p;
+            break;
+        }
+    }
+
+    if (processToFind == null)
+    {
+        AnsiConsole.Markup($"[red bold] Process name: {processName} not found in the trace! \n[/]");
+        return;
+    }
+
+    var managed = processToFind.LoadedDotNetRuntime();
+    var stats   = managed.GC.Stats();
+
+    var root = new Tree(processName);
+
+    // Total GC Pause Time (MSec)
+    var gcPauseTime = root.AddNode($"[green bold] Total Pause Time: {stats.TotalPauseTimeMSec.ToString("N2")} MSec [/]");
+    gcPauseTime.AddNode(new Table()
+        .RoundedBorder()
+        .AddColumn("GC Type")
+        .AddColumn("Total Pause Time (MSec)")
+        .AddRow("Gen0", $"{managed.GC.GCs.Where(gc => gc.Generation == 0).Sum(gc => gc.PauseDurationMSec).ToString("N2")}")
+        .AddRow("Gen1", $"{managed.GC.GCs.Where(gc => gc.Generation == 1).Sum(gc => gc.PauseDurationMSec).ToString("N2")}")
+        .AddRow("Gen2 Blocking", $"{managed.GC.GCs.Where(gc => gc.Generation == 2 && gc.Type != Microsoft.Diagnostics.Tracing.Parsers.Clr.GCType.BackgroundGC).Sum(gc => gc.PauseDurationMSec).ToString("N2")}")
+        .AddRow("BGC", $"{managed.GC.GCs.Where(gc => gc.Type == Microsoft.Diagnostics.Tracing.Parsers.Clr.GCType.BackgroundGC).Sum(gc => gc.PauseDurationMSec).ToString("N2")}")
+        );
+
+    // % Pause Time In GC
+    var gcPauseTimePercent = root.AddNode($"[green bold] % Pause Time In GC: {stats.GetGCPauseTimePercentage().ToString("N2")}% [/]");
+
+    // Heap Size Before (MB)
+    var heapSizeBefore = root.AddNode($"[green bold] Mean Heap Size Before: {managed.GC.GCs.Average(gc => gc.HeapSizeBeforeMB).ToString("N2")} MB [/]");
+
+    AnsiConsole.Write(root);
     AnsiConsole.WriteLine();
 }
 
@@ -172,11 +264,12 @@ void HandleAnalyzeProcess(string input)
 }
 
 AnsiConsole.Markup("[green bold underline] Welcome to the GC Chat! \n[/]");
-AnsiConsole.Markup("[green bold] To start a GC investigation, take a top level GC trace using PerfView with the following command: `perfview /GCCollectOnly /AcceptEULA /nogui collect` and press 's' after you have collected the trace. \n[/]");
-AnsiConsole.Markup("[green bold] \n Or call one of the following commands: \n[/]"); 
-AnsiConsole.Markup("[green bold] 1. Ask a GC related question to an AI bot. \n[/]");
-AnsiConsole.Markup("[green bold] 2. Enter /analyze and then specify a path to a trace to get a top level GC overview. \n[/]");
-AnsiConsole.Markup("[green bold] 3. Enter /analyze-process and then specify a path to a trace and a process name to get a top level GC overview. \n[/]");
+AnsiConsole.Markup("[green bold] Ask me a question! \n[/]");
+//AnsiConsole.Markup("[green bold] To start a GC investigation, take a top level GC trace using PerfView with the following command: `perfview /GCCollectOnly /AcceptEULA /nogui collect` and press 's' after you have collected the trace. \n[/]");
+//AnsiConsole.Markup("[green bold] \n Or call one of the following commands: \n[/]"); 
+//AnsiConsole.Markup("[green bold] 1. Ask a GC related question to an AI bot. \n[/]");
+//AnsiConsole.Markup("[green bold] 2. Enter /analyze and then specify a path to a trace to get a top level GC overview. \n[/]");
+//AnsiConsole.Markup("[green bold] 3. Enter /analyze-process and then specify a path to a trace and a process name to get a top level GC overview. \n[/]");
 //AnsiConsole.Markup("[green bold] 4. Enter /compare and then specify a path to two traces to get a top level GC comparison. \n[/]");
 AnsiConsole.Markup("[green bold] Ctrl+C to exit. \n[/]");
 
@@ -202,19 +295,85 @@ while (true)
         HandleAnalyzeProcess(input);
     }
 
-    else if (input.Contains("/compare"))
-    {
-
-    }
-
     else
     {
-        await AnsiConsole.Status()
-            .Spinner(Spinner.Known.Moon)
-            .StartAsync("The GC AI Bot is thinking..", async ctx =>
-            {
-                string result = await SendRequest(input);
-                AnsiConsole.WriteLine(result.Replace("\"", "").TrimStart());
-            });
+        await HandleModelRequest(input);
     }
+}
+
+static async Task HandleModelRequest(string input)
+{
+    await AnsiConsole.Status()
+        .Spinner(Spinner.Known.Moon)
+        .StartAsync("The GC AI Bot is thinking..", async ctx =>
+        {
+            string result = await SendModelRequest(input);
+            Console.WriteLine(result);
+            ModelResponse response = JsonConvert.DeserializeObject<ModelResponse>(result);
+            AnsiConsole.WriteLine(result.Replace("\"", "").TrimStart());
+
+            if (response.prompt_types.Count == 1)
+            {
+                string promptType = response.prompt_types.First();
+                switch (promptType)
+                {
+                    case "question": case "unknown":
+                        AnsiConsole.WriteLine(response.response_by_model);
+                        break;
+                    case "performance_issue":
+                        AnsiConsole.WriteLine($"I see you want to start a performance investigation for an issue of type: {response.performance_issue_type.Replace("_", " ")}, to get started with this please provide a top-level GC trace. If you are on windows, " +
+                            $"call the following perfview command and press 's' when you are done.: ``perfview /GCCollectOnly /AcceptEULA /nogui collect``. " +
+                            $"If you are on Linux, call the following command: dotnet trace collect -p <pid> -o <outputpath with .nettrace extension> --profile gc-collect --duration <in hh:mm:ss format>");
+                        break;
+                    case "analyze":
+                        {
+                            if (response.files_mentioned.Count == 0)
+                            {
+                                AnsiConsole.WriteLine(response.response_by_model);
+                            }
+
+                            else
+                            {
+                                if (string.IsNullOrEmpty(response.process_name))
+                                {
+                                    AnalyzeTrace(response.files_mentioned.First());
+                                }
+
+                                else
+                                {
+                                    AnalyzeProcess(response.files_mentioned.First(), response.process_name);
+                                }
+                            }
+
+                            break;
+                        }
+                }
+            }
+
+            // if performance_issue but no analyze.. ask for a trace.
+            // if question -> display response.
+            // if analyze -> call api on the file.
+
+            // If it's a single state => EASY.
+
+            // Multiple states - handle accordingly.
+
+            /*
+            foreach (var state in response.prompt_types)
+            {
+                switch (state)
+                {
+                    case "question": case "unknown":
+                        AnsiConsole.WriteLine(response.response_by_model);
+                        break;
+                    case "analyze":
+                        AnsiConsole.WriteLine($"Analyzing: {string.Join(", ", response.files_mentioned)}");
+                        break;
+                    case "performance_issue":
+                        AnsiConsole.WriteLine(response.response_by_model);
+                        break;
+                }
+            }
+            */
+        });
 }
